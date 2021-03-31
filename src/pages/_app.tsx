20686bed454c21c8311ea99c405e72d43b01ff87
r3nanp <renanmol87@gmail.com>
2021-03-31 09:34:39 -0300

    chore: add function types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
+import { ReactElement } from 'react'
 import { AppProps } from 'next/app'
 import '@styles/tailwind.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />
 }
 
