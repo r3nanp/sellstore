2d4673988950ab674aa6cce78e2a02c39e24ceb9
r3nanp <renanmol87@gmail.com>
2021-03-31 09:51:54 -0300

    chore: add jest configuration

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,7 @@ export default {
   coverageProvider: 'v8',
   preset: 'ts-jest',
   roots: ['<rootDir>'],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setupTests.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
