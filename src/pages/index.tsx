3be9cbe3b7b540a13b0b9578a863050587b5263f
r3nanp <renanmol87@gmail.com>
2021-04-14 12:03:14 -0300

    feat: add api data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,42 @@
 import { ReactElement } from 'react'
+import { GetStaticProps } from 'next'
+import Link from 'next/link'
+import { api } from 'services/api'
+
 import { Header } from '@components/Header/Header'
 import { SEO } from '@components/SEO'
+import { IProduct } from 'types/IProduct'
 
-export default function Home(): ReactElement {
+interface HomeProps {
+  products: IProduct[]
+}
+
+export default function Home({ products }: HomeProps): ReactElement {
   return (
     <>
-      <SEO title="SellStore | HomePage" />
+      <SEO title="SellStore | Homepage" />
 
       <Header />
+
+      {products.map((product, idx) => (
+        <div key={idx} className="">
+          <Link href={`/product/${product.id}`}>
+            <a>{product.name}</a>
+          </Link>
+        </div>
+      ))}
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const request = await api.get('/products')
+  const response = await request.data
+
+  return {
+    props: {
+      products: response
+    },
+    revalidate: 60 * 60 * 4
+  }
 }
