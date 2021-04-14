8f4f5660132f742d191cebc488124ce1ee533415
r3nanp <renanmol87@gmail.com>
2021-04-14 12:02:46 -0300

    feat: add providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { ReactElement } from 'react'
 import { AppProps } from 'next/app'
 import { CartProvider } from 'contexts/CartContext'
+import { AuthProvider } from 'contexts/AuthContext'
 import '@styles/tailwind.css'
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
   return (
-    <CartProvider>
-      <Component {...pageProps} />
-    </CartProvider>
+    <AuthProvider>
+      <CartProvider>
+        <Component {...pageProps} />
+      </CartProvider>
+    </AuthProvider>
   )
 }
 
