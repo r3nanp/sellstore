c0831473b66237fbc07fe2782faf92d2798db297
r3nanp <renanmol87@gmail.com>
2021-05-31 12:22:10 -0300

    chore: add new deps

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,8 +8,8 @@ export function Button({ children, ...rest }: ButtonProps): ReactElement {
   return (
     <>
       <button
+        className="w-full p-4 mt-6 shadow-outline-normal uppercase rounded text-secondary font-mono bg-black cursor-pointer border border-black transition hover:bg-white hover:text-primary"
         {...rest}
-        className="w-full p-4 mt-6 shadow-outline-normal uppercase rounded text-secondary font-mono font-bold bg-black cursor-pointer border border-black transition hover:bg-white hover:text-primary"
       >
         {children}
       </button>
