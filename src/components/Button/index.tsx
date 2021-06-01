dec406b4d6045fdf047c856d3588431a5795efa3
r3nanp <renanmol87@gmail.com>
2021-06-01 09:02:33 -0300

    refactor: remove fragment

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,13 +6,11 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export function Button({ children, ...rest }: ButtonProps): ReactElement {
   return (
-    <>
-      <button
-        className="w-full p-4 mt-6 shadow-outline-normal uppercase rounded text-secondary font-mono bg-black cursor-pointer border border-black transition hover:bg-white hover:text-primary"
-        {...rest}
-      >
-        {children}
-      </button>
-    </>
+    <button
+      className="w-full p-4 mt-6 shadow-outline-normal uppercase rounded text-secondary font-mono bg-black cursor-pointer border border-black transition hover:bg-white hover:text-primary"
+      {...rest}
+    >
+      {children}
+    </button>
   )
 }
