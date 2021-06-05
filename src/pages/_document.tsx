9fb375d5f48290139f356e8e5fcaa5671df11ff3
r3nanp <renanmol87@gmail.com>
2021-06-05 19:43:54 -0300

    chore: add script and icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,11 +6,16 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=Roboto&family=Roboto+Mono:wght@400;500&display=swap"
             rel="stylesheet"
           />
+          <script
+            crossOrigin="anonymous"
+            src="https://polyfill.io/v3/polyfill.min.js"
+          ></script>
         </Head>
         <body>
           <Main />
